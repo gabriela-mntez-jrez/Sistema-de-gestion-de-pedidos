b4698172e97b18a5739c9cc895392c644c608006
Gaby MJ <gabriela.mtnez.jrez@gmail.com>
2026-03-28 01:12:06 -0600

    Adding SwaggerConfig to avoid mixed content issue

---
+++ backend/src/main/java/com/sales/orders/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.sales.orders.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                // Avoid Mixed Content issues by setting the server URL to relative path
+                .servers(List.of(new Server().url("/")));
+    }
+}
